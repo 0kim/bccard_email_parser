10b5fe52e0855e138f521e29a32df3e257499394
ykim <me@younghun.kim>
2017-07-03 13:13:06 +0900

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
    name="BC Card Email Parser",
    version='0.1.1',
-   packages=['bccard_email_parser'],
+   packages=['BccardEmailParser'],
    author='youngkim',
    author_email='me@younghun.kim',
    url='https://github.com/0kim/bccard_email_parser',
